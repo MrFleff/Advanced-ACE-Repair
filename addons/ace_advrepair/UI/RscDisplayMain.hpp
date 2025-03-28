710bca69ecc3437c35ab8ec689f393c6e2c7ea90
Brett Mayson <brett@mayson.io>
2025-03-27 18:14:21 -0600

    fix config errors with hemtt

--- addons/ace_advrepair/UI/RscDisplayMain.hpp
+++ addons/ace_advrepair/UI/RscDisplayMain.hpp
@@ -9,109 +9,109 @@ class advrepair_dialog_Main
 		class Main_Frame: RscFrame
 		{
 			idc = 1800;
-			x = 0.346512 * safezoneW + safezoneX;
-			y = 0.291 * safezoneH + safezoneY;
-			w = 0.306977 * safezoneW;
-			h = 0.374 * safezoneH;
+			x = QUOTE(0.346512 * safezoneW + safezoneX);
+			y = QUOTE(0.291 * safezoneH + safezoneY);
+			w = QUOTE(0.306977 * safezoneW);
+			h = QUOTE(0.374 * safezoneH);
 		};
 		class Main_Background: RscText
 		{
 			idc = 1801;
-			x = 0.346512 * safezoneW + safezoneX;
-			y = 0.291 * safezoneH + safezoneY;
-			w = 0.306977 * safezoneW;
-			h = 0.374 * safezoneH;
+			x = QUOTE(0.346512 * safezoneW + safezoneX);
+			y = QUOTE(0.291 * safezoneH + safezoneY);
+			w = QUOTE(0.306977 * safezoneW);
+			h = QUOTE(0.374 * safezoneH);
 			colorBackground[] = {0, 0, 0, 0.7};
 		};
 		class Main_VehicleName: RscText
 		{
 			idc = 1000;
 			text = "Vehicle Name"; //--- ToDo: Localize;
-			x = 0.350349 * safezoneW + safezoneX;
-			y = 0.302 * safezoneH + safezoneY;
-			w = 0.19186 * safezoneW;
-			h = 0.044 * safezoneH;
-			sizeEx = 0.06 * safezoneH;
+			x = QUOTE(0.350349 * safezoneW + safezoneX);
+			y = QUOTE(0.302 * safezoneH + safezoneY);
+			w = QUOTE(0.19186 * safezoneW);
+			h = QUOTE(0.044 * safezoneH);
+			sizeEx = QUOTE(0.06 * safezoneH);
 		};
 		class Main_Notebox: RscEdit
 		{
 			idc = 1400;
-			x = 0.350349 * safezoneW + safezoneX;
-			y = 0.401 * safezoneH + safezoneY;
-			w = 0.299302 * safezoneW;
-			h = 0.253 * safezoneH;
+			x = QUOTE(0.350349 * safezoneW + safezoneX);
+			y = QUOTE(0.401 * safezoneH + safezoneY);
+			w = QUOTE(0.299302 * safezoneW);
+			h = QUOTE(0.253 * safezoneH);
 			lineSpacing = 1;
-			sizeEx = 0.025 * safezoneH;
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 		class Main_NoteboxSave: RscButton
 		{
 			idc = 1600;
 			onButtonClick = QUOTE([] call FUNC(guiSaveNote));
 			text = "Save Note"; //--- ToDo: Localize;
-			x = 0.469303 * safezoneW + safezoneX;
-			y = 0.357 * safezoneH + safezoneY;
-			w = 0.0613954 * safezoneW;
-			h = 0.033 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.469303 * safezoneW + safezoneX);
+			y = QUOTE(0.357 * safezoneH + safezoneY);
+			w = QUOTE(0.0613954 * safezoneW);
+			h = QUOTE(0.033 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 		class Main_PageChangeMiscParts: RscButton
 		{
 			idc = 1601;
 			text = "Minor Parts"; //--- ToDo: Localize;
-			x = 0.423256 * safezoneW + safezoneX;
-			y = 0.676 * safezoneH + safezoneY;
-			w = 0.072907 * safezoneW;
-			h = 0.033 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.423256 * safezoneW + safezoneX);
+			y = QUOTE(0.676 * safezoneH + safezoneY);
+			w = QUOTE(0.072907 * safezoneW);
+			h = QUOTE(0.033 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 		class Main_PageChangeMajorParts: RscButton
 		{
 			idc = 1602;
 			text = "Major Parts"; //--- ToDo: Localize;
-			x = 0.346512 * safezoneW + safezoneX;
-			y = 0.676 * safezoneH + safezoneY;
-			w = 0.072907 * safezoneW;
-			h = 0.033 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.346512 * safezoneW + safezoneX);
+			y = QUOTE(0.676 * safezoneH + safezoneY);
+			w = QUOTE(0.072907 * safezoneW);
+			h = QUOTE(0.033 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 		class Main_PageChangeFuelStats: RscButton
 		{
 			idc = 1603;
 			text = "Fuel Stats"; //--- ToDo: Localize;
-			x = 0.503837 * safezoneW + safezoneX;
-			y = 0.676 * safezoneH + safezoneY;
-			w = 0.072907 * safezoneW;
-			h = 0.033 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.503837 * safezoneW + safezoneX);
+			y = QUOTE(0.676 * safezoneH + safezoneY);
+			w = QUOTE(0.072907 * safezoneW);
+			h = QUOTE(0.033 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 		class Main_PageChangeAmmoStats: RscButton
 		{
 			idc = 1604;
 			text = "Ammo Stats"; //--- ToDo: Localize;
-			x = 0.580581 * safezoneW + safezoneX;
-			y = 0.676 * safezoneH + safezoneY;
-			w = 0.072907 * safezoneW;
-			h = 0.033 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.580581 * safezoneW + safezoneX);
+			y = QUOTE(0.676 * safezoneH + safezoneY);
+			w = QUOTE(0.072907 * safezoneW);
+			h = QUOTE(0.033 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
-		class Main_VehicleConfigStats: RscListbox
+		class Main_VehicleConfigStats: RscListBox
 		{
 			idc = 1500;
-			x = 0.546046 * safezoneW + safezoneX;
-			y = 0.302 * safezoneH + safezoneY;
-			w = 0.103605 * safezoneW;
-			h = 0.088 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.546046 * safezoneW + safezoneX);
+			y = QUOTE(0.302 * safezoneH + safezoneY);
+			w = QUOTE(0.103605 * safezoneW);
+			h = QUOTE(0.088 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 		class Main_NoteboxName: RscText
 		{
 			idc = 1001;
 			text = "Vehicle Notes:"; //--- ToDo: Localize;
-			x = 0.350349 * safezoneW + safezoneX;
-			y = 0.357 * safezoneH + safezoneY;
-			w = 0.103605 * safezoneW;
-			h = 0.033 * safezoneH;
-			sizeEx = 0.025 * safezoneH;
+			x = QUOTE(0.350349 * safezoneW + safezoneX);
+			y = QUOTE(0.357 * safezoneH + safezoneY);
+			w = QUOTE(0.103605 * safezoneW);
+			h = QUOTE(0.033 * safezoneH);
+			sizeEx = QUOTE(0.025 * safezoneH);
 		};
 	};
 };
